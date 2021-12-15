2fc9e597951dd51591f46c6129680f0a8f8e9b05
Julien Lin <lin.julien@hotmail.com>
2021-12-15 11:19:27 +0100

    d-15 part 2

--- 2021/d-15/solution.py
+++ 2021/d-15/solution.py
@@ -9,7 +9,23 @@ if __name__ == "__main__":
     with open("input.txt") as f:
         grid = [list(map(int, line.strip())) for line in f.readlines()]
 
+    len_grid= len(grid)
+    # print(len_grid)
+    n = 4
+    for line in grid:
+        buf = list(map(lambda x: x+1 if x+1 < 10 else 1, line))
+        for _ in range(n):
+            line += buf
+            buf = list(map(lambda x: x+1 if x+1 < 10 else 1, buf))
+
+    for _ in range(n):
+        for line in grid[-len_grid:]:
+            buf = list(map(lambda x: x+1 if x+1 < 10 else 1, line))
+            grid.append(buf)
+
+
     len_col = len(grid[0])
+    # print(len(grid), len_col)
     for i in range(len(grid)):
         for j in range(len_col):
             beg = i * len_col + j
@@ -38,4 +54,4 @@ if __name__ == "__main__":
 
     print(dijkstra_path_length(G, 0, len(grid) * len_col - 1))
     shortest_path = dijkstra_path(G, 0, len(grid) * len_col - 1)
-    print(shortest_path, len(shortest_path))
+    # print(shortest_path, len(shortest_path))
