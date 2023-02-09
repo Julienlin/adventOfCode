8f8627f2da0e831816c17b61219659f0e1459730
Julien Lin <lin.julien@hotmail.com>
2023-02-09 00:05:23 -0500

    d-14

--- 2022/d-14/Main.java
+++ 2022/d-14/Main.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("test.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
             List<List<int[]>> lines = new ArrayList<>();
             String line;
             int minWidth = 500;
@@ -41,10 +41,13 @@ public class Main {
 
             }
 
-            System.out.println(String.format("minWidth: %d, maxWidth: %d, maxHeight: %d", minWidth, maxWidth, maxHeight));
+            System.out
+                    .println(String.format("minWidth: %d, maxWidth: %d, maxHeight: %d", minWidth, maxWidth, maxHeight));
 
             Simulator simulator = new Simulator(minWidth, maxWidth, maxHeight, lines);
-
+            System.out.println(simulator);
+            int tours = simulator.simulate();
+            System.out.println(tours);
 
         } catch (Exception e) {
             // TODO: handle exception
@@ -57,15 +60,16 @@ public class Main {
 
 class Simulator {
 
-    int X, Y;
+    int ROWS, COLS;
     int minWidth;
     int maxWidth;
     int maxHeight;
+    int[] source;
 
-    boolean[][] isOccupied;
+    int[][] isOccupied;
 
-    static final int COL = 1;
-    static final int ROW = 0;
+    static final int COL = 0;
+    static final int ROW = 1;
 
     public Simulator(int minWidth, int maxWidth, int maxHeight, List<List<int[]>> lines) {
 
@@ -76,12 +80,16 @@ class Simulator {
         this.maxWidth = maxWidth;
         this.minWidth = minWidth;
 
-        X = maxWidth - minWidth;
-        Y = maxHeight;
+        source = new int[2];
+        source[ROW] = 0;
+        source[COL] = 500 - minWidth;
 
-        System.out.println(String.format("X: %d, Y: %d", X, Y));
+        COLS = maxWidth - minWidth + 1;
+        ROWS = maxHeight + 1;
 
-        isOccupied = new boolean[Y][X];
+        // System.out.println(String.format("X: %d, Y: %d", ROWS, COLS));
+
+        isOccupied = new int[ROWS][COLS];
 
         for (List<int[]> line : lines) {
             int[] pointA = line.get(0);
@@ -91,19 +99,74 @@ class Simulator {
                 int row = pointA[ROW];
                 int leftMost = Math.min(pointA[COL], pointB[COL]) - minWidth;
                 int rightmost = Math.max(pointA[COL], pointB[COL]) - minWidth;
-                for (int i = leftMost; i < rightmost; i++) {
-                    isOccupied[row][i] = true;
+                for (int i = leftMost; i <= rightmost; i++) {
+                    System.out.println(String.format("row: %d, col: %d", row, i));
+                    isOccupied[row][i] = 1;
                 }
             } else {
                 int col = pointA[COL] - minWidth;
                 int lessHigh = Math.min(pointA[ROW], pointB[ROW]);
                 int highest = Math.max(pointA[ROW], pointB[ROW]);
-                for (int i = lessHigh; i < highest; i++) {
-                    isOccupied[i][col] = true;
+                for (int i = lessHigh; i <= highest; i++) {
+                    isOccupied[i][col] = 1;
                 }
             }
         }
 
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Integer.toString(isOccupied[0][0]);
+        for (int i = 0; i < isOccupied.length; i++) {
+            sb.append(String.join(" ", Arrays.stream(isOccupied[i]).mapToObj(String::valueOf).toArray(String[]::new)));
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    int simulate() {
+        int tours = 0;
+        boolean isInfinite = false;
+        int[] sand;
+        boolean isMoving;
+        while (!isInfinite) {
+            sand = source.clone();
+            // System.out.println(String.format("sand: %d, %d", sand[ROW], sand[COL]));
+            isMoving = true;
+            while (isMoving) {
+                // System.out.println(String.join(" ", "Moving", String.valueOf(sand[ROW] + 1),
+                // String.valueOf(sand[COL] + 1), String.valueOf(sand[COL] - 1)));
+                if (sand[ROW] + 1 > maxHeight || sand[COL] + 1 > maxWidth || sand[COL] - 1 < 0) {
+                    isInfinite = true;
+                    isMoving = false;
+                    // System.out.println(String.join(" ", "coucou", String.valueOf(sand[ROW] + 1 >=
+                    // maxHeight),
+                    // String.valueOf(sand[COL] + 1 >= maxWidth), String.valueOf(sand[COL] - 1 <=
+                    // minWidth)));
+                    // System.out.println(String.join(" ", "coucou", String.valueOf(sand[ROW] + 1),
+                    // String.valueOf(sand[COL] + 1), String.valueOf(sand[COL] - 1)));
+                } else if (isOccupied[sand[ROW] + 1][sand[COL]] < 1) {
+                    sand[ROW]++;
+                } else if (isOccupied[sand[ROW] + 1][sand[COL] - 1] < 1) {
+                    sand[COL]--;
+                    sand[ROW]++;
+                } else if (isOccupied[sand[ROW] + 1][sand[COL] + 1] < 1) {
+                    sand[COL]++;
+                    sand[ROW]++;
+                } else {
+                    isOccupied[sand[ROW]][sand[COL]] = 2;
+                    isMoving = false;
+                }
+            }
+            tours++;
+            // System.out.println(this);
+            // System.out.println();
+        }
+
+        return tours - 1;
     }
 
 }
