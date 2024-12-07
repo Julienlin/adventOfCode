b3cd92b5591eeb113a6c6fdebda21ca675161c29
Julien Lin <julien.l@padoa-group.com>
2024-12-07 19:18:23 +0100

    WIP

--- 2024/d-05/main.go
+++ 2024/d-05/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"container/list"
 	"context"
 	"fmt"
 	"os"
@@ -30,6 +31,12 @@ func main() {
 	}
 	defer f.Close()
 
+	// part1(f)
+	// f.Seek(0, 0)
+	part2(f)
+}
+
+func part1(f *os.File) {
 	scanner := bufio.NewScanner(f)
 
 	rules := make(Rule)
@@ -38,7 +45,6 @@ func main() {
 		line := scanner.Text()
 
 		if line == "" {
-			// end of the first part
 			break
 		}
 
@@ -46,9 +52,9 @@ func main() {
 		first := items[0]
 		second := items[1]
 
-		if rules[second] == nil {
-			rules[second] = make([]string, 0, 2)
-		}
+		// if rules[second] == nil {
+		// 	rules[second] = make([]string, 0, 2)
+		// }
 
 		rules[first] = append(rules[first], second)
 	}
@@ -168,4 +174,198 @@ func incrementCounter(page []string, counter *uint64) error {
 	fmt.Println("middleValue", middleValue, len(page)/2, page)
 	atomic.AddUint64(counter, middleValue)
 	return nil
+}
+
+func part2(f *os.File) {
+	scanner := bufio.NewScanner(f)
+	rules := make(map[int][]int)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			break
+		}
+
+		items := strings.Split(line, "|")
+
+		first, err := strconv.Atoi(items[0])
+		if err != nil {
+			panic(err)
+		}
+		second, err := strconv.Atoi(items[1])
+		if err != nil {
+			panic(err)
+		}
+
+		rules[first] = append(rules[first], second)
+
+	}
+
+	pages := make([][]int, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+		page := strings.Split(line, ",")
+
+		pageInt := make([]int, 0, len(page))
+		for _, p := range page {
+			n, _ := strconv.Atoi(p)
+			pageInt = append(pageInt, n)
+		}
+		pages = append(pages, pageInt)
+	}
+
+	if scanner.Err() != nil {
+		panic(scanner.Err())
+	}
+
+	var count uint64
+
+	wg := sync.WaitGroup{}
+
+	for _, page := range pages {
+		page := page
+		ctx, cancelBase := context.WithCancel(context.TODO())
+
+		pairWithIndex := producePairsWithIndex(ctx, page)
+		swapInstructions := filterUnsatifiedRule(ctx, rules, pairWithIndex)
+		reorderedPage := fixPageOrder(ctx, page, swapInstructions)
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			defer cancelBase()
+			getValueInt(ctx, &count, reorderedPage)
+		}()
+	}
+
+	wg.Wait()
+
+	fmt.Println("part 2", count)
+}
+
+func producePairsWithIndex(ctx context.Context, page []int) <-chan []int {
+	out := make(chan []int)
+
+	go func() {
+		defer close(out)
+		fmt.Println(page)
+		for i := 0; i < len(page)-1; i++ {
+			for j := i + 1; j < len(page); j++ {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					p := []int{page[i], page[j], i, j}
+					// fmt.Println(p)
+					out <- p
+				}
+			}
+		}
+	}()
+
+	return out
+}
+
+func filterUnsatifiedRule(ctx context.Context, rules map[int][]int, pairsWithIndexChan <-chan []int) <-chan []int {
+	out := make(chan []int)
+
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case pairWithIndex, ok := <-pairsWithIndexChan:
+				if ok {
+					if slices.Contains(rules[pairWithIndex[1]], pairWithIndex[0]) {
+						// fmt.Println(pairWithIndex, rules[pairWithIndex[1]])
+						out <- []int{pairWithIndex[2], pairWithIndex[3]}
+					}
+				} else {
+					return
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return out
+}
+
+func fixPageOrder(ctx context.Context, page []int, indexToSwap <-chan []int) <-chan []int {
+	out := make(chan []int)
+
+	// pageCopy := make([]int, len(page))
+	// copy(pageCopy, page)
+
+	elements := make([]*list.Element, 0, len(page))
+
+	linkedlist := list.New()
+
+	for _, p := range page {
+		el := linkedlist.PushBack(p)
+		elements = append(elements, el)
+	}
+
+	go func() {
+		hasChanged := false
+		for {
+			select {
+			case pair, ok := <-indexToSwap:
+				if ok {
+					fmt.Println(pair)
+					i := pair[0]
+					j := pair[1]
+					// pageCopy[i], pageCopy[j] = pageCopy[j], pageCopy[i]
+					if !isElementAfterMark(elements[i], elements[j]) {
+						linkedlist.MoveAfter(elements[i], elements[j])
+						hasChanged = true
+					}
+				} else {
+					indexToSwap = nil
+					if hasChanged {
+
+						pageCopy := make([]int, 0, len(page))
+
+						for current := linkedlist.Front(); current != nil; current = current.Next() {
+							pageCopy = append(pageCopy, current.Value.(int))
+						}
+						fmt.Println("before", page)
+						fmt.Println("after ", pageCopy)
+						out <- pageCopy
+					}
+					close(out)
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	return out
+}
+
+func isElementAfterMark(el *list.Element, mark *list.Element) bool {
+	for cur := mark; cur != nil; cur = cur.Next() {
+		if cur == el {
+			return true
+		}
+	}
+	return false
+}
+
+func getValueInt(ctx context.Context, count *uint64, pageChan <-chan []int) {
+	for {
+		select {
+		case page, ok := <-pageChan:
+			if ok {
+				// fmt.Println(page)
+				middleValue := page[len(page)/2]
+				atomic.AddUint64(count, uint64(middleValue))
+			} else {
+				return
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
 }
